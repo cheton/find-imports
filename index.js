0c28ca6148f3dcff22791c4e5ed60c85ce9985b0
Cheton Wu <cheton@gmail.com>
2016-05-20 18:08:43 +0800

    Add options to allow absolute and relative imports

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ var babel = require('babel-core');
 var esprima = require('esprima');
 var glob = require('glob');
 
-var findImports = function(patterns) {
+// @params {string|array} patterns The glob pattern or a list of glob patterns
+// @params {object} options The options object
+// @params {boolean} [options.absoluteImports] True to return absolute imports, defaults to false
+// @params {boolean} [options.relativeImports] True to return relative imports, defaults to false
+var findImports = function(patterns, options) {
     var requiredModules = {};
     var filenames = [];
 
+    // patterns
     patterns = [].concat(patterns || []);
     patterns.forEach(function(pattern) {
         filenames = filenames.concat(glob.sync(pattern));
     });
+
+    // options
+    options = options || {};
 
     filenames.forEach(function(filename) {
         try {
@@ -20,14 +28,36 @@ var findImports = function(patterns) {
             requiredModules[filename] = [];
 
             tree.body.forEach(function(node) {
-                if (node.type === 'VariableDeclaration') {
+                if (node.type === 'ExpressionStatement') {
+                    if (node.expression.type !== 'CallExpression' ||
+                        node.expression.callee.name !== 'require') {
+                        return;
+                    }
+
+                    var value = node.expression.arguments[0].value;
+                    if (value[0] === '/' && !options.absoluteImports) {
+                        return;
+                    }
+                    if (value[0] === '.' && !options.relativeImports) {
+                        return;
+                    }
+                    requiredModules[filename].push(value);
+                } else if (node.type === 'VariableDeclaration') {
                     node.declarations.forEach(function(decl) {
-                        if (decl.init &&
-                            decl.init.type === 'CallExpression' &&
-                            decl.init.callee.name === 'require' &&
-                            decl.init.arguments[0].value.indexOf('/') === -1) {
-                            requiredModules[filename].push(decl.init.arguments[0].value);
+                        if (!decl.init ||
+                            decl.init.type !== 'CallExpression' ||
+                            decl.init.callee.name !== 'require') {
+                            return;
                         }
+
+                        var value = decl.init.arguments[0].value;
+                        if (value[0] === '/' && !options.absoluteImports) {
+                            return;
+                        }
+                        if (value[0] === '.' && !options.relativeImports) {
+                            return;
+                        }
+                        requiredModules[filename].push(value);
                     });
                 }
             });
